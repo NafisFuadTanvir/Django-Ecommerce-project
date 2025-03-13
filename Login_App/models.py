7688cbcbced57a0d0ba31caef69b87ef0d247558
NafisFuadTanvir <nafisf026@gmail.com>
2025-03-13 12:17:52 +0600

    create custom profile model with one to one relation

--- Login_App/models.py
+++ Login_App/models.py
@@ -55,4 +55,28 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def get_short_name(self):
         return self.email
-        
+
+
+class Profile(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    username = models.CharField(max_length=264, blank=True)
+    full_name = models.CharField(max_length=264, blank=True)
+    address_1 = models.TextField(max_length=300, blank=True)
+    city = models.CharField(max_length=40, blank=True)
+    zipcode = models.CharField(max_length=10, blank=True)
+    country = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True) 
+    
+    def __str__(self):
+        return self.user.username + "'s Profile"
+
+    def is_fully_filled(self):
+        field_names = [f.name for f in self._meta.get_fields()]
+    
+        for field_name in field_names:
+            value = getattr(self, field_name)
+            if value is None or value == '':
+             return False
+            
+        return True       
