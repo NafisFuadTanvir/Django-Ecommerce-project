528effa57448b1565d81f39a0f22289799afc46b
NafisFuadTanvir <nafisf026@gmail.com>
2025-04-16 05:26:45 +0800

    added the order model and runs the migrations and register the models in admin

--- Order_App/models.py
+++ Order_App/models.py
@@ -26,4 +26,20 @@ class Cart(models.Model):
         total = self.item.price * self.quantity
         float_total = format(total, '.2f')
         return float(float_total)
-    
+
+class Order(models.Model):
+    orderitems = models.ManyToManyField(Cart)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    paymentId = models.CharField(max_length=264, blank=True, null=True)
+    orderId = models.CharField(max_length=200, blank=True, null=True)
+
+    def get_totals(self):
+        total = 0
+        for order_item in self.orderitems.all():
+            total += float(order_item.get_total())
+        return total    
